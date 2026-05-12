98bbb06ec4453c4f87b245f17b416e7de0194893
Alzatiyo <140862803+Alzatiyo@users.noreply.github.com>
2026-05-12 14:28:54 -0500

    Delete ScrapingRunt directory

--- ScrapingRunt/funtions/convertir_fecha.py
+++
@@ -1,9 +0,0 @@
-def convertir_fecha(fecha):
-    if not fecha:
-        return ""
-
-    try:
-        dia, mes, anio = fecha.split("/")
-        return f"{anio}{mes}{dia}"
-    except:
-        return ""
